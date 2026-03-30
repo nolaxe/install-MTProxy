c66006f5573eb7d33f0d9bd354fb38d7371f7d01
nolaxe <54135136+nolaxe@users.noreply.github.com>
2026-03-30 21:57:43 +0500

    Update tmp3.sh

--- tmp3.sh
+++ tmp3.sh
@@ -268,7 +268,7 @@ main_menu() {
     echo -e "${NC}\nBuild from existing image: $IMAGE_NAME"
     echo -e " 1) ${CYAN}Fast Install             (Port: $PORT, Domain: $SITE)${NC}"
     echo -e " 2) Custom Install           (Custom Port, Domain...)"
-    echo -e " 3) ${YELLOW}${TOGGLE_ACTION} ${NC}           $STATUS_MSG"
+    echo -e " 3) ${YELLOW}${TOGGLE_ACTION} ${NC}           +$STATUS_MSG"
     echo -e " 4) ${RED}Full Uninstall${NC}           (Stop & Remove All)\n"
     echo -e " 5) Run external build script: $SCRIPT_NAME)"
     echo -ne "\n${YELLOW}[?] Choose option [1-5]:${NC} "
