b3b71774e7a518f1ec93da242aa4e266e8685f0b
nolaxe <54135136+nolaxe@users.noreply.github.com>
2026-04-02 13:08:28 +0500

    Update telemt-from-image-mu-sp-rew5.sh

--- telemt-from-image-mu-sp-rew5.sh
+++ telemt-from-image-mu-sp-rew5.sh
@@ -250,7 +250,7 @@ status_detection() {
         TOGGLE_ACTION="Not installed"
         EXISTING_LINK="" # In this case, the link is truly not needed
     fi
-    local inst_date=""; [ -f ".install_date" ] && inst_date="Last setup date: -$(cat .install_date)"
+    local inst_date=""; [ -f ".install_date" ] && inst_date="Last setup date: $(cat .install_date)"
     DOCKER_INFO="\n CURRENT STATUS\n$inst_date\nConfig files [${INST_ICON}] |  Active [${ACT_ICON}]"
 }
 
